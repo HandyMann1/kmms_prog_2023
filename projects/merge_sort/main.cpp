62d3f8e7ba45fc1616bb550835a800b0c43f4451
Artyom Arshinov <artyomarshinov@gmail.com>
2024-02-19 14:17:36 +0300

    [merge_sort]add merge_sort

---
+++ projects/merge_sort/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+void merge(std::vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    std::vector<int> L(n1);
+    std::vector<int> R(n2);
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = arr[left + i];
+    }
+
+    for (int i = 0; i < n2; i++) {
+        R[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0;
+    int j = 0;
+    int k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(std::vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    std::vector<int> arr = { 12, 11, 13, 5, 6, 7 };
+
+    int arrSize = arr.size();
+
+    std::cout << "Исходный массив: ";
+    for (int i = 0; i < arrSize; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    mergeSort(arr, 0, arrSize - 1);
+
+    std::cout << "\nОтсортированный массив: ";
+    for (int i = 0; i < arrSize; i++) {
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
